2aad41e3f35eeada331fbcdb577fe74ad5812e4b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-08 17:06:41 +0200

    fix bug of message

--- assets/functions.php
+++ assets/functions.php
@@ -1,9 +1,14 @@
 <?php
 $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?$%€&*#';
-$password_length = isset($_GET['password']) ? intval($_GET['password']) : 0;;
 
-if ($password_length > 0) {
-    $password = substr(str_shuffle($characters), 0, $password_length);
-} elseif ($password_length === 0) {
-    $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
-};
+
+if (isset($_GET['password'])) {
+    if ($_GET['password'] == '') {
+        $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
+    } else {
+        $password_length = intval($_GET['password']);
+        $password = substr(str_shuffle($characters), 0, $password_length);
+    }
+} else {
+    $password = '';
+}
