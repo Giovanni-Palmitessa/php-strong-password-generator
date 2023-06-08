5741345268bbd964f64eb38d6810bde1e9c141a6
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-08 17:20:52 +0200

    add if with condition 0

--- assets/functions.php
+++ assets/functions.php
@@ -2,13 +2,30 @@
 $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?$%€&*#';
 
 
+// if (isset($_GET['password'])) {
+//     if ($_GET['password'] == '') {
+//         $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
+//     } else {
+//         $password_length = intval($_GET['password']);
+//         $password = substr(str_shuffle($characters), 0, $password_length);
+//     }
+// } else {
+//     $password = '';
+// }
+
 if (isset($_GET['password'])) {
     if ($_GET['password'] == '') {
         $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
     } else {
         $password_length = intval($_GET['password']);
         $password = substr(str_shuffle($characters), 0, $password_length);
-    }
+        if ($password_length > 0) {
+            $password_length = intval($_GET['password']);
+            $password = substr(str_shuffle($characters), 0, $password_length);
+        } elseif ($password_length === 0) {
+            $password = 'Hai inserito 0 come valore. Inserisci un numero maggiore.';
+        };
+    };
 } else {
     $password = '';
 }
