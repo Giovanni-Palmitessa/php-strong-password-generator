6499a3b370d55a8f683815f666418bb95704b3e4
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-08 16:17:59 +0200

    remove comments from index

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-// $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?$%€&*#';
-// $password_length = isset($_GET['password']) ? intval($_GET['password']) : 0;;
-
-// if ($password_length > 0) {
-//     $password = substr(str_shuffle($characters), 0, $password_length);
-// } elseif ($password_length === 0) {
-//     $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
-// };
 include_once __DIR__ . '/assets/functions.php'
 ?>
 
