bc052787215ed82e05fc6ae2fe53c30beccc545f
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-08 17:21:09 +0200

    removed useless comments

--- assets/functions.php
+++ assets/functions.php
@@ -1,18 +1,6 @@
 <?php
 $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?$%€&*#';
 
-
-// if (isset($_GET['password'])) {
-//     if ($_GET['password'] == '') {
-//         $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
-//     } else {
-//         $password_length = intval($_GET['password']);
-//         $password = substr(str_shuffle($characters), 0, $password_length);
-//     }
-// } else {
-//     $password = '';
-// }
-
 if (isset($_GET['password'])) {
     if ($_GET['password'] == '') {
         $password = 'Non hai inserito nessun numero. Inserisci per generare la password';
